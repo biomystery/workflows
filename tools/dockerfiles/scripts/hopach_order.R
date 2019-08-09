1809335a0ff924b4fddcb29ea1e119d8bcad807d
Michael Kotliar <misha.kotliar@gmail.com>
2019-08-09 12:31:34 -0400

    Update hopach script and dockerfile

--- tools/dockerfiles/scripts/hopach_order.R
+++ tools/dockerfiles/scripts/hopach_order.R
@@ -60,16 +60,19 @@ load_data_set <- function(filenames, suffixes, target_colname, intersect_by) {
 
 # Parser
 parser <- ArgumentParser(description='Hopach Ordering')
-parser$add_argument("-i", "--input",        help='Input CSV/TSV genelist files',              type="character", required="True", nargs='+')
-parser$add_argument("-n", "--name",         help='Names, the order corresponds to input. Default: basename of --input files', type="character", nargs='+')
-parser$add_argument("-t", "--target",       help='Target column name to be used by Hopach. Default: Rpkm', type="character", default="Rpkm")
-parser$add_argument("-c", "--combine",      help='Combine inputs by columns names. Default: RefseqId, GeneId, Chrom, TxStart, TxEnd, Strand', type="character", nargs='+', default=c("RefseqId", "GeneId", "Chrom", "TxStart", "TxEnd", "Strand"))
-parser$add_argument("-d", "--dist",         help='Distance metric. Default: cosangle',        type="character", choices=c("cosangle","abscosangle","euclid","abseuclid","cor","abscor"), default="cosangle")
-parser$add_argument("-l", "--logtransform", help='Log2 transform input data prior running hopach. Default: false',    action='store_true')
-parser$add_argument("-k", "--keep",         help='Keep discarded values at the end of the file. Default: false',      action='store_true')
-parser$add_argument("-m", "--min",          help='Min value for target column value. Default: 0',     type="double",    default=0)
-parser$add_argument("-p", "--palette",      help='Palette color names. Default: black, red, yellow', type="character", nargs='+', default=c("black", "red", "yellow"))
-parser$add_argument("-o", "--output",       help='Output prefix. Default: ordered_genelist',          type="character", default="./ordered_genelist")
+parser$add_argument("--input",        help='Input CSV/TSV genelist files',              type="character", required="True", nargs='+')
+parser$add_argument("--name",         help='Names, the order corresponds to input. Default: basename of --input files', type="character", nargs='+')
+parser$add_argument("--target",       help='Target column name to be used by Hopach. Default: Rpkm', type="character", default="Rpkm")
+parser$add_argument("--combine",      help='Combine inputs by columns names. Default: RefseqId, GeneId, Chrom, TxStart, TxEnd, Strand', type="character", nargs='+', default=c("RefseqId", "GeneId", "Chrom", "TxStart", "TxEnd", "Strand"))
+parser$add_argument("--dist",         help='Distance metric. Default: cosangle',        type="character", choices=c("cosangle","abscosangle","euclid","abseuclid","cor","abscor"), default="cosangle")
+parser$add_argument("--logtransform", help='Log2 transform input data prior running hopach. Default: false',    action='store_true')
+parser$add_argument("--keep",         help='Keep discarded values at the end of the file. Default: false',      action='store_true')
+parser$add_argument("--heatmap",      help='Export heatmap to png. Default: false',                  action='store_true')
+parser$add_argument("--distmatrix",   help='Export distance matrix to png. Default: false',          action='store_true')
+parser$add_argument("--variability",  help='Export clsuter variability plot to png. Default: false', action='store_true')
+parser$add_argument("--min",          help='Min value for target column value. Default: 0',     type="double",    default=0)
+parser$add_argument("--palette",      help='Palette color names. Default: black, red, yellow', type="character", nargs='+', default=c("black", "red", "yellow"))
+parser$add_argument("--output",       help='Output prefix. Default: ordered_genelist',          type="character", default="./ordered_genelist")
 args <- parser$parse_args(commandArgs(trailingOnly = TRUE))
 
 
@@ -124,28 +127,54 @@ print("Run hopach")
 hopach_results <- hopach(expression_data, dmat=distance_matrix)
 
 
-# Create and export distance matrix to png file 
-distance_matrix_name = paste(args$output, "_dist_matrix.png", sep="")
-png(distance_matrix_name,
-    width = 5*300,
-    height = 5*300,
-    res = 300,
-    pointsize = 8)
-dplot(distance_matrix, hopach_results, ord="cluster", main="Distance Matrix", col=colorRampPalette(args$palette)(n = 299))
+# Export distance matrix to png file
+if(args$distmatrix){
+    print("Build distance matrix plot")
+    distance_matrix_name = paste(args$output, "_dist_matrix.png", sep="")
+    png(distance_matrix_name,
+        width = 5*300,
+        height = 5*300,
+        res = 300,
+        pointsize = 8)
+    dplot(distance_matrix, hopach_results, ord="final", main="Distance Matrix", showclusters=FALSE, col=colorRampPalette(args$palette)(n = 299))
+    print(paste("Export distance matrix plot to ", distance_matrix_name, sep=""))
+}
+
+
+# Estimate the variability of the hopach clusters (boostrap resampling)
+if(args$variability){
+    print("Estimate cluster variability")
+    variability_plot_name = paste(args$output, "_variablility.png", sep="")
+    png(variability_plot_name,
+        width = 5*300,
+        height = 5*300,
+        res = 300,
+        pointsize = 8)
+    bobstrap_resampling <- boothopach(expression_data, hopach_results, hopachlabels=TRUE)
+    bootplot(bobstrap_resampling, hopach_results, ord="final", main="Cluster variability", showclusters=FALSE)
+    print(paste("Export cluster variability plot to ", variability_plot_name, sep=""))
+}
 
 
 # Apply order from hopach clustering results to the expression data
-expression_data <- expression_data[hopach_results$clustering$order,]
+expression_data <- expression_data[hopach_results$final$order,]
 
 
 # Select rows from original data applying the order of expression data
 result_data <- original_data[rownames(expression_data),]
 
 
+# Append main clusters labels
+main_clusters = as.data.frame(hopach_results$clustering$labels)
+colnames(main_clusters) = "cluster"
+main_clusters <- cbind(main_clusters, "L" = outer(main_clusters$cluster, 10^c((nchar(trunc(main_clusters$cluster))[1]-1):0), function(a, b) a %/% b %% 10))
+result_data <- cbind(result_data, main_clusters[, c(2:ncol(main_clusters))])
+
+
 # Add discarded rows at the bottom of result data
-if (args$keep)
-{
+if (args$keep){
     print("Append discarded data")
+    original_data[,colnames(main_clusters[, c(2:ncol(main_clusters))])] <- 0
     discarded_data = original_data[!rownames(original_data) %in% rownames(expression_data),]
     result_data <- rbind(result_data, discarded_data)
 }
@@ -159,23 +188,24 @@ write.table(result_data,
             row.names=FALSE,
             col.names=TRUE,
             quote=FALSE)
+print(paste("Export hopach results raw data to ", raw_data_name, sep=""))
 
 
 # Create and export heatmap to png file
-print("Generate heatmap")
-heatmap_name = paste(args$output, "_heatmap.png", sep="")
-pheatmap(data.matrix(result_data[,(length(args$combine)+1):ncol(result_data)]),
-         cluster_rows=FALSE,
-         cluster_cols=FALSE,
-         color=colorRampPalette(args$palette)(n = 299),
-         scale="row",
-         border_color=FALSE,
-         show_rownames=FALSE,
-         filename=heatmap_name)
+if (args$heatmap){
+    print("Generate heatmap")
+    heatmap_name = paste(args$output, "_heatmap.png", sep="")
+    pheatmap(data.matrix(result_data[rownames(expression_data),(length(args$combine)+1):(ncol(result_data)-ncol(main_clusters)+1)]),
+            cluster_rows=FALSE,
+            cluster_cols=FALSE,
+            color=colorRampPalette(args$palette)(n = 299),
+            scale="row",
+            border_color=FALSE,
+            show_rownames=FALSE,
+            filename=heatmap_name)
+    print(paste("Export heatmap to ", heatmap_name, sep=""))
+}
 
-
-print(paste("Hopach results are saved to ", raw_data_name, sep=""))
-print(paste("Heatmap is saved to ", heatmap_name, sep=""))
-print(paste("Distance matrix is saved to ", distance_matrix_name, sep=""))
-
-dev.off()
+if (args$distmatrix || args$variability){
+    dev.off()
+}
