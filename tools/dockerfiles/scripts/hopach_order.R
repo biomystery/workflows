da4cb152ea65f391ece688a8bbf10daf5d3b0990
Michael Kotliar <misha.kotliar@gmail.com>
2019-08-15 19:14:10 -0400

    Update hopach clustering script and dockerfile

--- tools/dockerfiles/scripts/hopach_order.R
+++ tools/dockerfiles/scripts/hopach_order.R
@@ -21,6 +21,9 @@ suppressMessages(library(pheatmap))
 # *.tsv - TSV
 # CSV is used by default
 #
+# if --method is "both" heatmap is built using expression_data_row. The column order is
+# taken from the expression_data_col.
+#
 ##########################################################################################
 
 
@@ -58,24 +61,106 @@ load_data_set <- function(filenames, suffixes, target_colname, intersect_by) {
 }
 
 
+cluster <- function(expression_data, logtransform, center, normalize, dist, output, suffix, transpose) {
+
+    if (transpose){
+        print("   Transpose expression data")
+        expression_data = t(expression_data)
+    }
+
+    if (logtransform) { 
+        print("   Log2 transform expression data")
+        if (any(expression_data==0)){
+            print(paste("Zero values are replaced by ", 1, sep=""))
+            expression_data[expression_data==0] <- 1
+        }
+        expression_data = log2(expression_data)
+    }
+
+    if (!is.null(center)) {
+        print(paste("   Center expression data by ", center, sep=""))
+        if (center == "mean"){
+            expression_data = expression_data - rowMeans(expression_data)    
+        } else {
+            expression_data = expression_data - rowMedians(data.matrix(expression_data))    
+        }
+    }
+
+    if (normalize) {
+        print("   Normalize expression data")
+        std = sqrt(rowSums(expression_data^2))
+        expression_data = expression_data/std
+        expression_data = replace(expression_data, is.na(expression_data), 0)
+    }
+
+    print("   Create distance matrix")
+    distance_matrix <- distancematrix(expression_data, dist)
+
+    print("   Run hopach clustering")
+    hopach_results <- hopach(expression_data, dmat=distance_matrix)
+
+    distance_matrix_name = paste(output, "_", suffix, "_dist_matrix.png", sep="")
+    png(distance_matrix_name,
+        width = 5*300,
+        height = 5*300,
+        res = 300,
+        pointsize = 8)
+    dplot(distance_matrix, hopach_results, ord="final", main=paste("Distance matrix (", dist, ", ", suffix, ")", sep=""), showclusters=FALSE, col=colorRampPalette(args$palette)(n = 299))
+    print(paste("   Save distance matrix plot to ", distance_matrix_name, sep=""))
+
+    print("   Reorder expression data")
+    expression_data <- expression_data[hopach_results$final$order,]
+    
+    if (transpose){
+        print("   Transpose expression data")
+        expression_data = t(expression_data)
+    }
+
+    labels_df = as.data.frame(hopach_results$final$labels)
+    colnames(labels_df) = "label"
+    labels_df = cbind(labels_df, "L" = outer(labels_df$label, 10^c((nchar(trunc(labels_df$label))[1]-1):0), function(a, b) a %/% b %% 10))
+    labels_df = labels_df[, c(-1), drop = FALSE]
+
+    return( list(expression=expression_data, labels=labels_df) )
+
+}
+
 # Parser
-parser <- ArgumentParser(description='Hopach Ordering: filter, log transform, center, normalize, cluster, append discarded (optional)')
-parser$add_argument("--input",        help='Input CSV/TSV genelist files',              type="character", required="True", nargs='+')
-parser$add_argument("--name",         help='Names, the order corresponds to input. Default: basename of --input files', type="character", nargs='+')
-parser$add_argument("--target",       help='Target column name to be used by Hopach. Default: Rpkm', type="character", default="Rpkm")
-parser$add_argument("--combine",      help='Combine inputs by columns names. Default: RefseqId, GeneId, Chrom, TxStart, TxEnd, Strand', type="character", nargs='+', default=c("RefseqId", "GeneId", "Chrom", "TxStart", "TxEnd", "Strand"))
-parser$add_argument("--dist",         help='Distance metric. Default: cosangle',        type="character", choices=c("cosangle","abscosangle","euclid","abseuclid","cor","abscor"), default="cosangle")
-parser$add_argument("--logtransform", help='Log2 transform input data prior running hopach. Default: false',    action='store_true')
-parser$add_argument("--center",       help='Center rows. Default: not centered', type="character", choices=c("mean", "median"))
-parser$add_argument("--norm",         help='Normalize rows. Default: not normalized', action='store_true' )
-parser$add_argument("--reordercol",   help='Reorder heatmap columns. Default: false', action='store_true' )
-parser$add_argument("--keep",         help='Keep discarded values at the end of the file. Default: false',      action='store_true')
-parser$add_argument("--heatmap",      help='Export heatmap to png. Default: false',                  action='store_true')
-parser$add_argument("--distmatrix",   help='Export distance matrix to png. Default: false',          action='store_true')
-parser$add_argument("--variability",  help='Export clsuter variability plot to png. Default: false', action='store_true')
-parser$add_argument("--min",          help='Min value for target column value. Default: 0',     type="double",    default=0)
-parser$add_argument("--palette",      help='Palette color names. Default: black, red, yellow', type="character", nargs='+', default=c("black", "red", "yellow"))
-parser$add_argument("--output",       help='Output prefix. Default: ordered_genelist',          type="character", default="./ordered_genelist")
+parser <- ArgumentParser(description='Hopach Clustering')
+parser$add_argument("--input",           help='Input CSV/TSV files',                                    type="character", required="True", nargs='+')
+parser$add_argument("--name",            help='Input aliases, the order corresponds to --input order. Default: basename of --input files', type="character", nargs='+')
+parser$add_argument("--target",          help='Target column to be used by Hopach. Default: Rpkm',      type="character", default="Rpkm")
+parser$add_argument("--combine",         help='Combine inputs by columns names. Default: RefseqId, GeneId, Chrom, TxStart, TxEnd, Strand', type="character", nargs='+', default=c("RefseqId", "GeneId", "Chrom", "TxStart", "TxEnd", "Strand"))
+parser$add_argument("--method",          help='Cluster method. Default: both',                          type="character", choices=c("row","column","both"), default="both")
+parser$add_argument("--palette",         help='Palette color names. Default: red, black, green',        type="character", nargs='+', default=c("red", "black", "green"))
+parser$add_argument("--output",          help='Output prefix. Default: hopach',                         type="character", default="./hopach")
+
+
+parser$add_argument("--rowmin",          help='Exclude rows from clustering by the min value of a target column. Default: 0',          type="double",    default=0)
+parser$add_argument("--rowkeep",         help='Append excluded rows to the result table after clustering is finished. Default: false', action='store_true')
+
+
+parser$add_argument("--rowdist",         help='Distance metric for row clustering. Default: cosangle',  type="character", choices=c("cosangle","abscosangle","euclid","abseuclid","cor","abscor"), default="cosangle")
+parser$add_argument("--coldist",         help='Distance metric for column clustering. Default: euclid', type="character", choices=c("cosangle","abscosangle","euclid","abseuclid","cor","abscor"), default="euclid")
+
+
+parser$add_argument("--rowlogtransform", help='Log2 transform input data prior to running row clustering. Default: false',    action='store_true')
+parser$add_argument("--collogtransform", help='Log2 transform input data prior to running column clustering. Default: false', action='store_true')
+
+
+parser$add_argument("--rowcenter",       help='Center rows prior to running clustering. Default: not centered',    type="character", choices=c("mean", "median"))
+parser$add_argument("--colcenter",       help='Center columns prior to running clustering. Default: not centered', type="character", choices=c("mean", "median"))
+
+
+parser$add_argument("--rownorm",         help='Normalize rows to prior running clustering. Default: not normalized',    action='store_true' )
+parser$add_argument("--colnorm",         help='Normalize columns to prior running clustering. Default: not normalized', action='store_true' )
+
+
+parser$add_argument("--heatmap",         help='Save heatmap. Default: false',                action='store_true')
+
+
+
+
 args <- parser$parse_args(commandArgs(trailingOnly = TRUE))
 
 
@@ -87,135 +172,107 @@ if(is.null(args$name)){
 }
 
 
-# Load and combine data
-original_data <- load_data_set(args$input, args$name, args$target, args$combine)
-selected_columns = c((length(args$combine)+1):ncol(original_data))
+# Load and combine input data
+input_data <- load_data_set(args$input, args$name, args$target, args$combine)
+expression_columns = c((length(args$combine)+1):ncol(input_data))
 
-# Get row names to be filtered out from the original data after all transformation are done
-print(paste("Apply filter to input data ", args$target, " >= ", args$min, sep=""))
-filtered_data_row_names <- rownames(original_data[!rowSums(original_data[,selected_columns] < args$min),])
-print(paste("Number of rows after filtering ", length(filtered_data_row_names), sep=""))
+# Get rows to be filtered out from input data
+print(paste("Apply filter to input data ", args$target, " >= ", args$rowmin, sep=""))
+filtered_rows <- rownames(input_data[!rowSums(input_data[,expression_columns] < args$rowmin),])
+print(paste("Number of rows after filtering ", length(filtered_rows), sep=""))
 
-
-# Log2 transform original data
-if (args$logtransform) { 
-    print("Log2 transform input data")
-    if (any(original_data[,selected_columns]==0)){
-        print(paste("Zero values are replaced by ", 1, sep=""))
-        original_data[,selected_columns][original_data[,selected_columns]==0] <- 1
-    }
-    original_data[,selected_columns] = log2(original_data[,selected_columns])
-}
-
-
-# Center original data by mean or median
-if (!is.null(args$center)) {
-    print(paste("Center input data by ", args$center, sep=""))
-    if (args$center == "mean"){
-        original_data[,selected_columns] = original_data[,selected_columns] - rowMeans(original_data[,selected_columns])    
-    } else {
-        original_data[,selected_columns] = original_data[,selected_columns] - rowMedians(data.matrix(original_data[,selected_columns]))    
-    }
-}
-
-
-# Normalize original data
-if (args$norm) {
-    print("Normalize input data")
-    std = sqrt(rowSums(original_data[,selected_columns]^2))
-    original_data[,selected_columns] = original_data[,selected_columns]/std
-    original_data = replace(original_data, is.na(original_data), 0)
-}
-
-
-# Extract expression data from transformed original data
+# Extract expression data
 print("Extract expression data")
-expression_data = original_data[filtered_data_row_names, selected_columns]
+expression_data = input_data[filtered_rows, expression_columns]
 
 
-# Create distance matrix
-print("Create distance matrix")
-distance_matrix <- distancematrix(expression_data, args$dist)
+if (args$method == "both" || args$method == "row"){
+    print("Clustering by row")
+    cluster_row = cluster(expression_data, args$rowlogtransform, args$rowcenter, args$rownorm, args$rowdist, args$output, "row", FALSE)
+    expression_data_row = cluster_row$expression
+    labels_row = cluster_row$labels
+}
 
-
-# Run hopach clustering
-print("Run hopach")
-hopach_results <- hopach(expression_data, dmat=distance_matrix)
-
-
-# Export distance matrix to png file
-if(args$distmatrix){
-    print("Build distance matrix plot")
-    distance_matrix_name = paste(args$output, "_dist_matrix.png", sep="")
-    png(distance_matrix_name,
-        width = 5*300,
-        height = 5*300,
-        res = 300,
-        pointsize = 8)
-    dplot(distance_matrix, hopach_results, ord="cluster", main=paste("Distance matrix (", args$dist, ")", sep=""), showclusters=FALSE, col=colorRampPalette(args$palette)(n = 299))
-    print(paste("Export distance matrix plot to ", distance_matrix_name, sep=""))
+if (args$method == "both" || args$method == "column"){
+    print("Clustering by columns")
+    cluster_col = cluster(expression_data, args$collogtransform, args$colcenter, args$colnorm, args$coldist, args$output, "column", TRUE)
+    expression_data_col = cluster_col$expression
+    labels_col = cluster_col$labels
 }
 
 
-# Estimate the variability of the hopach clusters (boostrap resampling)
-if(args$variability){
-    print("Estimate cluster variability")
-    variability_plot_name = paste(args$output, "_variablility.png", sep="")
-    png(variability_plot_name,
-        width = 5*300,
-        height = 5*300,
-        res = 300,
-        pointsize = 8)
-    bobstrap_resampling <- boothopach(expression_data, hopach_results, hopachlabels=TRUE)
-    bootplot(bobstrap_resampling, hopach_results, ord="cluster", main=paste("Cluster variability (", args$dist, ")", sep=""), showclusters=FALSE)
-    print(paste("Export cluster variability plot to ", variability_plot_name, sep=""))
+# Reorder rows and columns of input data
+if (args$method == "both"){
+    output_data <- input_data[rownames(expression_data_row), append(colnames(input_data)[1:length(args$combine)], colnames(expression_data_col))]
+} else if (args$method == "row"){
+    output_data <- input_data[rownames(expression_data_row),]
+} else if (args$method == "column"){
+    output_data <- input_data[, append(colnames(input_data)[1:length(args$combine)], colnames(expression_data_col))]
 }
 
 
-# Apply order from main hopach clusters to the expression data
-expression_data <- expression_data[hopach_results$clustering$order,]
-
-
-# Select rows from original data applying the order of expression data
-result_data <- original_data[rownames(expression_data),]
-
-
-# Append main clusters labels
-main_clusters = as.data.frame(hopach_results$clustering$labels)
-colnames(main_clusters) = "cluster"
-main_clusters <- cbind(main_clusters, "L" = outer(main_clusters$cluster, 10^c((nchar(trunc(main_clusters$cluster))[1]-1):0), function(a, b) a %/% b %% 10))
-result_data <- cbind(result_data, main_clusters[, c(2:ncol(main_clusters)), drop = FALSE])
-
-
-# Add discarded rows at the bottom of result data
-if (args$keep){
-    print("Append discarded data")
-    original_data[,colnames(main_clusters[, c(2:ncol(main_clusters)), drop = FALSE])] <- 0
-    discarded_data = original_data[!rownames(original_data) %in% rownames(expression_data),]
-    result_data <- rbind(result_data, discarded_data)
+# Append row cluster labels
+if (args$method == "both" || args$method == "row"){
+    output_data <- cbind(output_data, labels_row)
 }
 
 
-# Export raw result data as a text file
-raw_data_name = paste(args$output, "_raw_data.tsv", sep="")
-write.table(result_data,
-            file=raw_data_name,
+
+# Add discarded rows at the bottom of output data
+if (args$rowkeep){
+    print("Append discarded rows at the bottom of the output table")
+    if (args$method == "both" || args$method == "column"){
+        discarded_data = input_data[!rownames(input_data) %in% filtered_rows, append(colnames(input_data)[1:length(args$combine)], colnames(expression_data_col))]
+    } else {
+        discarded_data = input_data[!rownames(input_data) %in% filtered_rows,]   
+    }
+    if (args$method == "both" || args$method == "row"){
+        discarded_data[, colnames(labels_row)] <- 0
+    }
+    output_data <- rbind(output_data, discarded_data)
+}
+
+
+# Save main clustering results
+output_data_name = paste(args$output, "_clustering.tsv", sep="")
+write.table(output_data,
+            file=output_data_name,
             sep="\t",
             row.names=FALSE,
             col.names=TRUE,
             quote=FALSE)
-print(paste("Export hopach results raw data to ", raw_data_name, sep=""))
+print(paste("Export hopach clustering results to ", output_data_name, sep=""))
+
+
+# Save column cluster labels
+if (args$method == "both" || args$method == "column"){
+    labels_col = cbind(experiment = colnames(expression_data_col), labels_col)
+    labels_col_name = paste(args$output, "_column_clustering_labels.tsv", sep="")
+    write.table(labels_col,
+                file=labels_col_name,
+                sep="\t",
+                row.names=FALSE,
+                col.names=TRUE,
+                quote=FALSE)
+    print(paste("Export hopach column clustering results to ", labels_col_name, sep=""))
+}
 
 
 # Create and export heatmap to png file
 if (args$heatmap){
-    print("Generate heatmap")
     heatmap_name = paste(args$output, "_heatmap.png", sep="")
-    pheatmap(data.matrix(result_data[rownames(expression_data),(length(args$combine)+1):(ncol(result_data)-ncol(main_clusters)+1)]),
-            cluster_rows=FALSE,
-            cluster_cols=args$reordercol,
+    if (args$method == "both"){
+        heatmap_data <- data.matrix(expression_data_row[,colnames(expression_data_col)])
+    } else if (args$method == "row"){
+        heatmap_data <- data.matrix(expression_data_row)
+    } else if (args$method == "column"){
+        heatmap_data <- data.matrix(expression_data_col)
+    }
+    pheatmap(heatmap_data,
+            cluster_row=FALSE,
+            cluster_cols=FALSE,
             treeheight_col = 0,
-            main = paste("Heatmap (", args$dist, ")", sep=""),
+            main = paste("Heatmap (clustering: ", args$method, ")", sep=""),
             color=colorRampPalette(args$palette)(n = 299),
             scale="none",
             border_color=FALSE,
@@ -224,6 +281,5 @@ if (args$heatmap){
     print(paste("Export heatmap to ", heatmap_name, sep=""))
 }
 
-if (args$distmatrix || args$variability){
-    dev.off()
-}
+
+dev.off()
