ed5514d7a24d7630d34811a6ecedb3f0458cef83
Michael Kotliar <misha.kotliar@gmail.com>
2020-01-08 12:52:18 -0500

    Update quantile normalization script

--- devel/miraldi/quantile_normalize.R
+++ devel/miraldi/quantile_normalize.R
@@ -8,9 +8,10 @@ suppressMessages(library("preprocessCore"))
 suppressMessages(library("rtracklayer"))
 
 
-load_coverage_data <- function(filenames, bin_size) {
+load_coverage_data <- function(filenames, bin_size, sum_signal) {
     collected_binned_data = NULL
     previous_sequence_info = NULL
+    previous_strand_info = NULL
     for (i in 1:length(filenames)) {
         raw_coverage_data <- import.bw(filenames[i])
         sequence_info = seqinfo(raw_coverage_data)
@@ -27,6 +28,12 @@ load_coverage_data <- function(filenames, bin_size) {
                                                    "score"))
         binned_data = binned_data[,c("seqnames", "start", "end", "strand", "score")]
         print(paste("Loaded ", nrow(binned_data), " ranges from ", filenames[i], " (bin size = ", bin_size, ")", sep=""))
+        if (is.null(previous_strand_info)){
+            previous_strand_info = binned_data[,"strand"]
+        } else if (previous_strand_info != binned_data[,"strand"]) {
+            print("Exiting: strand info is different for input bigwig files")
+            quit(save = "no", status = 1, runLast = FALSE)
+        }
         score_column_name = paste(i, "score", sep="_")
         strand_column_name = paste(i, "strand", sep="_")
         colnames(binned_data)[colnames(binned_data) == "score"] = score_column_name
@@ -34,8 +41,13 @@ load_coverage_data <- function(filenames, bin_size) {
         if (is.null(collected_binned_data)){
             collected_binned_data = binned_data
         } else {
-            collected_binned_data[,score_column_name] = binned_data[,score_column_name]
-            collected_binned_data[,strand_column_name] = binned_data[,strand_column_name]
+            if(sum_signal){
+                print("Add signal to the previous bigwig")
+                collected_binned_data[,"1_score"] = collected_binned_data[,"1_score"] + binned_data[,score_column_name]
+            } else {
+                collected_binned_data[,score_column_name] = binned_data[,score_column_name]
+                collected_binned_data[,strand_column_name] = binned_data[,strand_column_name]
+            }
         }
     }
     return( list(coverage_data=collected_binned_data, sequence_info=previous_sequence_info) )
@@ -70,15 +82,25 @@ build_plots <- function(score_data, colors, reference_score_data){
 
 }
 
-export_results <- function(coverage_data, sequence_info, filenames, prefix, suffix){
-    for (i in 1:length(filenames)) {
-        output_filename = paste(prefix, head(unlist(strsplit(basename(filenames[i]), ".", fixed = TRUE)), 1), suffix, ".bigwig", sep="")
-        print(paste("Export normalized bigwig to", output_filename, sep=" "))
+export_results <- function(coverage_data, sequence_info, filenames, prefix, suffix, sum_signal){
+    if (sum_signal){
+        output_filename = paste(prefix, "combined", suffix, ".bigwig", sep="")
+        print(paste("Export combined normalized bigwig to", output_filename, sep=" "))
         df = coverage_data[,c(1,2,3)]
-        df[,"score"] = coverage_data[, paste(i, "score", sep="_")]
-        df[,"strand"] = coverage_data[, paste(i, "strand", sep="_")]
+        df[,"score"] = coverage_data[,"1_score"]
+        df[,"strand"] = coverage_data[,"1_strand"]
         gr = makeGRangesFromDataFrame(df, seqinfo=sequence_info, keep.extra.columns=TRUE)
         export.bw(gr, output_filename)
+    } else {
+        for (i in 1:length(filenames)) {
+            output_filename = paste(prefix, head(unlist(strsplit(basename(filenames[i]), ".", fixed = TRUE)), 1), suffix, ".bigwig", sep="")
+            print(paste("Export normalized bigwig to", output_filename, sep=" "))
+            df = coverage_data[,c(1,2,3)]
+            df[,"score"] = coverage_data[, paste(i, "score", sep="_")]
+            df[,"strand"] = coverage_data[, paste(i, "strand", sep="_")]
+            gr = makeGRangesFromDataFrame(df, seqinfo=sequence_info, keep.extra.columns=TRUE)
+            export.bw(gr, output_filename)
+        }
     }
 }
 
@@ -91,6 +113,7 @@ get_args <- function(){
     parser$add_argument("--palette",   help='Palette color names. Default: red, black, green, yellow, blue, pink, brown', type="character", nargs='+', default=c("red", "black", "green", "yellow", "blue", "pink", "brown"))
     parser$add_argument("--prefix",    help='Output prefix for normalized files. Default: ./', type="character", default="./")
     parser$add_argument("--suffix",    help='Output suffix for normalized files. Default: _normalized', type="character", default="_normalized")
+    parser$add_argument("--sum",       help='Sum signal from input bigwigs before running normalization. Default: False', action='store_true' )
     args <- parser$parse_args(commandArgs(trailingOnly = TRUE))
     return (args)
 }
@@ -104,13 +127,13 @@ png(filename=paste(args$prefix, "plot", args$suffix, "_%03d.png", sep=""))
 
 # Load coverage data from bigwigs
 print(paste("Load data from", length(args$input), "bigwig(s) for quantile normalization",sep=" "))
-raw_data <- load_coverage_data(args$input, args$bin)
+raw_data <- load_coverage_data(args$input, args$bin, args$sum)
 coverage_data = raw_data$coverage_data
 sequence_info = raw_data$sequence_info
 
 # Extract score data
 print("Extract score data")
-score_columns = unlist(lapply(seq(1, length(args$input)), paste, "score", sep="_"))
+score_columns = grep("score", colnames(coverage_data), value = TRUE, ignore.case = TRUE)
 score_data = coverage_data[,score_columns, drop = FALSE]
 
 
@@ -129,7 +152,7 @@ if (!is.null(args$reference)){
     }
     norm_score_data = as.data.frame(normalize.quantiles.use.target(as.matrix(score_data), reference_score_data[,1]))
 } else {
-    print("Normalize score data between all input samples")
+    print("Normalize score data between input samples")
     norm_score_data = as.data.frame(normalize.quantiles(as.matrix(score_data)))
 }
 
@@ -142,5 +165,5 @@ build_plots(score_data, args$palette, reference_score_data)
 build_plots(norm_score_data, args$palette, reference_score_data)
 
 # Save normalized data to bigwigs
-export_results(coverage_data, sequence_info, args$input, args$prefix, args$suffix)
+export_results(coverage_data, sequence_info, args$input, args$prefix, args$suffix, args$sum)
 
