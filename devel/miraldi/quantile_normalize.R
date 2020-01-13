11ad5f6add639fb39f186d5c42d2411e6c5aed9f
Michael Kotliar <misha.kotliar@gmail.com>
2020-01-12 19:04:56 -0500

    Update normalization script

--- devel/miraldi/quantile_normalize.R
+++ devel/miraldi/quantile_normalize.R
@@ -8,96 +8,68 @@ suppressMessages(library("preprocessCore"))
 suppressMessages(library("rtracklayer"))
 
 
-load_coverage_data <- function(filenames, bin_size, sum_signal) {
+DATA_COLUMNS  = c("seqnames", "start", "end", "strand", "score")
+INTERSECT_BY  = c("seqnames", "start", "end", "strand")
+INP_SCORE     = "inp_score"
+REF_SCORE     = "ref_score"
+SUM_SCORE     = "sum_score"
+SEQ_COLUMNS   = c("chr", "length")
+
+
+load_coverage_data <- function(filenames, bin_size, score_column) {
     collected_binned_data = NULL
-    previous_sequence_info = NULL
-    previous_strand_info = NULL
+    collected_sequence_lengths = NULL
     for (i in 1:length(filenames)) {
         raw_coverage_data <- import.bw(filenames[i])
-        sequence_info = seqinfo(raw_coverage_data)
-        if (is.null(previous_sequence_info)){
-            previous_sequence_info = sequence_info
-        } else if (seqnames(previous_sequence_info)!=seqnames(sequence_info) || seqlengths(previous_sequence_info)!=seqlengths(sequence_info)) {
-            print("Exiting: sequence info is different for input bigwig files")
-            quit(save = "no", status = 1, runLast = FALSE)
-        }
-        binned_data <- as.data.frame(binnedAverage(tileGenome(seqlengths=sequence_info,
-                                                              tilewidth=bin_size,
-                                                              cut.last.tile.in.chrom=TRUE) ,
-                                                   coverage(raw_coverage_data, weight="score"),
-                                                   "score"))
-        binned_data = binned_data[,c("seqnames", "start", "end", "strand", "score")]
+        binned_data = as.data.frame(binnedAverage(tileGenome(seqlengths=seqinfo(raw_coverage_data), tilewidth=bin_size, cut.last.tile.in.chrom=TRUE), coverage(raw_coverage_data, weight="score"), "score"))
+        binned_data = binned_data[,DATA_COLUMNS]
+        colnames(binned_data)[colnames(binned_data) == "score"] = paste(i, score_column, sep="_")
         print(paste("Loaded ", nrow(binned_data), " ranges from ", filenames[i], " (bin size = ", bin_size, ")", sep=""))
-        if (is.null(previous_strand_info)){
-            previous_strand_info = binned_data[,"strand"]
-        } else if (previous_strand_info != binned_data[,"strand"]) {
-            print("Exiting: strand info is different for input bigwig files")
-            quit(save = "no", status = 1, runLast = FALSE)
-        }
-        score_column_name = paste(i, "score", sep="_")
-        strand_column_name = paste(i, "strand", sep="_")
-        colnames(binned_data)[colnames(binned_data) == "score"] = score_column_name
-        colnames(binned_data)[colnames(binned_data) == "strand"] = strand_column_name
+
+        sequence_lengths = t(data.frame(as.list(seqlengths(seqinfo(raw_coverage_data)))))
+        sequence_lengths = data.frame("_" = rownames(sequence_lengths), sequence_lengths)
+        rownames(sequence_lengths) = NULL
+        colnames(sequence_lengths) = SEQ_COLUMNS
+        print("Chromosome list")
+        print(sequence_lengths)
+
         if (is.null(collected_binned_data)){
             collected_binned_data = binned_data
+            collected_sequence_lengths = sequence_lengths
         } else {
-            if(sum_signal){
-                print("Add signal to the previous bigwig")
-                collected_binned_data[,"1_score"] = collected_binned_data[,"1_score"] + binned_data[,score_column_name]
-            } else {
-                collected_binned_data[,score_column_name] = binned_data[,score_column_name]
-                collected_binned_data[,strand_column_name] = binned_data[,strand_column_name]
-            }
+            print("Merging data")
+            collected_binned_data = merge(collected_binned_data, binned_data, by=INTERSECT_BY, sort = FALSE)
+            collected_sequence_lengths = merge(collected_sequence_lengths, sequence_lengths, by=SEQ_COLUMNS, sort = FALSE)
         }
     }
-    return( list(coverage_data=collected_binned_data, sequence_info=previous_sequence_info) )
+    return( list(binned_data=collected_binned_data, sequence_lengths=collected_sequence_lengths) )
 }
 
 
-build_plots <- function(score_data, colors, reference_score_data){
-    icolor <- colorRampPalette(colors)(length(colnames(score_data))+1)
-    plot(density(score_data[,1]), col=icolor[1], lwd=3, main="Density plot of intensities", ylab="Density", xlab="Coverage")
-    legend_names = colnames(score_data[,1, drop = FALSE])
-    legend_colors = c(icolor[1])
-    if (ncol(score_data) > 1){
-        for(i in 2:ncol(score_data)){
-            lines(density(score_data[,i]), col=icolor[i], lwd=3)
-            legend_names = append(legend_names, colnames(score_data[,i, drop = FALSE]))
-            legend_colors = append(legend_colors, icolor[i])
-        }
-    }
+build_boxplots <- function(score_data, reference_score_data){
+    legend_names = colnames(score_data)
     if (!is.null(reference_score_data)){
-        lines(density(reference_score_data[,1]), col=icolor[length(icolor)], lwd=3)
         legend_names = append(legend_names, "reference")
-        legend_colors = append(legend_colors, icolor[length(icolor)])
-    }
-    legend("topleft", legend=legend_names, col=legend_colors, lwd=3)
-    
-    if (!is.null(reference_score_data)){
         boxplot(cbind(score_data, reference_score_data), main="Distribution boxplot", names=legend_names, outline=FALSE)
     } else {
         boxplot(score_data, main="Distribution boxplot", names=legend_names, outline=FALSE)
     }
-    
-
 }
 
 export_results <- function(coverage_data, sequence_info, filenames, prefix, suffix, sum_signal){
-    if (sum_signal){
+    if (sum_signal && length(filenames) > 1){
         output_filename = paste(prefix, "combined", suffix, ".bigwig", sep="")
         print(paste("Export combined normalized bigwig to", output_filename, sep=" "))
-        df = coverage_data[,c(1,2,3)]
-        df[,"score"] = coverage_data[,"1_score"]
-        df[,"strand"] = coverage_data[,"1_strand"]
+        df = coverage_data[,INTERSECT_BY]
+        df[,"score"] = coverage_data[,SUM_SCORE]
         gr = makeGRangesFromDataFrame(df, seqinfo=sequence_info, keep.extra.columns=TRUE)
         export.bw(gr, output_filename)
     } else {
         for (i in 1:length(filenames)) {
             output_filename = paste(prefix, head(unlist(strsplit(basename(filenames[i]), ".", fixed = TRUE)), 1), suffix, ".bigwig", sep="")
             print(paste("Export normalized bigwig to", output_filename, sep=" "))
-            df = coverage_data[,c(1,2,3)]
-            df[,"score"] = coverage_data[, paste(i, "score", sep="_")]
-            df[,"strand"] = coverage_data[, paste(i, "strand", sep="_")]
+            df = coverage_data[,INTERSECT_BY]
+            df[,"score"] = coverage_data[, paste(i, INP_SCORE, sep="_")]
             gr = makeGRangesFromDataFrame(df, seqinfo=sequence_info, keep.extra.columns=TRUE)
             export.bw(gr, output_filename)
         }
@@ -110,7 +82,6 @@ get_args <- function(){
     parser$add_argument("--input",     help='Input bigwig files to be normalized', type="character", required="True", nargs='+')
     parser$add_argument("--reference", help='Opitional input reference bigwig file to be used as reference distribution', type="character")
     parser$add_argument("--bin",       help='Bin size for normalization. We will take mean of a bin before normalization. Default: 50', type="integer", default=50)
-    parser$add_argument("--palette",   help='Palette color names. Default: red, black, green, yellow, blue, pink, brown', type="character", nargs='+', default=c("red", "black", "green", "yellow", "blue", "pink", "brown"))
     parser$add_argument("--prefix",    help='Output prefix for normalized files. Default: ./', type="character", default="./")
     parser$add_argument("--suffix",    help='Output suffix for normalized files. Default: _normalized', type="character", default="_normalized")
     parser$add_argument("--sum",       help='Sum signal from input bigwigs before running normalization. Default: False', action='store_true' )
@@ -127,43 +98,75 @@ png(filename=paste(args$prefix, "plot", args$suffix, "_%03d.png", sep=""))
 
 # Load coverage data from bigwigs
 print(paste("Load data from", length(args$input), "bigwig(s) for quantile normalization",sep=" "))
-raw_data <- load_coverage_data(args$input, args$bin, args$sum)
-coverage_data = raw_data$coverage_data
-sequence_info = raw_data$sequence_info
+raw_data <- load_coverage_data(args$input, args$bin, INP_SCORE)
+coverage_data = raw_data$binned_data
+sequence_lengths = raw_data$sequence_lengths
 
-# Extract score data
-print("Extract score data")
-score_columns = grep("score", colnames(coverage_data), value = TRUE, ignore.case = TRUE)
-score_data = coverage_data[,score_columns, drop = FALSE]
+if (length(args$input) > 1){
+    print(paste("Number of common ranges", nrow(coverage_data), sep=" "))
+    print("Common chromosome list")
+    print(sequence_lengths)
+}
 
+
+if (args$sum && length(args$input) > 1){
+    print("Sum score signal from the loaded bigwigs")
+    coverage_data[,SUM_SCORE] = rowSums(coverage_data[, !colnames(coverage_data) %in% INTERSECT_BY])
+    coverage_data = coverage_data[,c(INTERSECT_BY,SUM_SCORE)]
+}
 
 
 # Normalize score data
 reference_score_data = NULL
 if (!is.null(args$reference)){
     print(paste("Normalize score data based on reference distribution from", args$reference, sep=" "))
-    reference_data <- load_coverage_data(args$reference, args$bin)
-    reference_coverage_data = reference_data$coverage_data
-    reference_sequence_info = reference_data$sequence_info
-    reference_score_data = reference_coverage_data[,"1_score", drop = FALSE]
-    if (seqnames(reference_sequence_info)!=seqnames(sequence_info) || seqlengths(reference_sequence_info)!=seqlengths(sequence_info)) {
-        print("Exiting: reference sequence info is different from the sequence info of loaded bigwig files")
-        quit(save = "no", status = 1, runLast = FALSE)
-    }
+    reference_raw_data = load_coverage_data(args$reference, args$bin, REF_SCORE)
+    reference_coverage_data <- reference_raw_data$binned_data
+    reference_sequence_lengths = reference_raw_data$sequence_lengths
+    
+    print("Intersect ranges and chromosome lists between input and reference data")
+    coverage_data = merge(coverage_data, reference_coverage_data, by=INTERSECT_BY, sort = FALSE)
+    sequence_lengths = merge(sequence_lengths, reference_sequence_lengths, by=SEQ_COLUMNS, sort = FALSE)
+    print(head(coverage_data))
+
+    reference_coverage_data = coverage_data[, !colnames(coverage_data) %in% c(grep(INP_SCORE, colnames(coverage_data), value=TRUE, ignore.case=TRUE), SUM_SCORE)]
+    print(head(reference_coverage_data))
+    coverage_data = coverage_data[, !colnames(coverage_data) %in% c(grep(REF_SCORE, colnames(coverage_data), value=TRUE, ignore.case=TRUE))]
+    print(head(coverage_data))
+
+    
+    print(paste("Number of ranges common with reference data", nrow(coverage_data), sep=" "))
+    print("Chromosome list common for all bigwig files and reference")
+    print(sequence_lengths)
+
+    print("Extract score data from bigwigs")
+    score_data = coverage_data[, !colnames(coverage_data) %in% INTERSECT_BY, drop = FALSE]
+    print("Extract score data from reference ditribution")
+    reference_score_data = reference_coverage_data[, !colnames(reference_coverage_data) %in% INTERSECT_BY, drop = FALSE]
+
     norm_score_data = as.data.frame(normalize.quantiles.use.target(as.matrix(score_data), reference_score_data[,1]))
 } else {
+    print("Extract score data from bigwigs")
+    score_data = coverage_data[, !colnames(coverage_data) %in% INTERSECT_BY, drop = FALSE]
     print("Normalize score data between input samples")
     norm_score_data = as.data.frame(normalize.quantiles(as.matrix(score_data)))
 }
 
 colnames(norm_score_data) = colnames(score_data)
-rownames(norm_score_data) = rownames(score_data)
-coverage_data[,score_columns] = norm_score_data
 
 print("Build plots")
-build_plots(score_data, args$palette, reference_score_data)
-build_plots(norm_score_data, args$palette, reference_score_data)
+build_boxplots(score_data, reference_score_data)
+build_boxplots(norm_score_data, reference_score_data)
+
 
 # Save normalized data to bigwigs
+print("Export results")
+coverage_data[,!colnames(coverage_data) %in% INTERSECT_BY] = norm_score_data
+
+print(head(coverage_data))
+sequence_info = Seqinfo(seqnames=as.character(sequence_lengths[,SEQ_COLUMNS[1]]),
+                        seqlengths=sequence_lengths[,SEQ_COLUMNS[2]],
+                        isCircular=rep(FALSE, nrow(sequence_lengths)),
+                        genome="custom")
 export_results(coverage_data, sequence_info, args$input, args$prefix, args$suffix, args$sum)
 
